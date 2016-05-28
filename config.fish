5b27099e061db27600b72aca945acc8f8287d6b2
John Palmer <jpal8929@physics.usyd.edu.au>
2016-05-28 18:53:38 +1000

    rm autpjump - not needed with fish

--- config.fish
+++ config.fish
@@ -9,7 +9,6 @@ abbr -a nmatlab matlab -nodesktop -nosplash
 abbr -a sl ls
 abbr -a top htop
 if status --is-interactive
-    . /etc/profile.d/autojump.fish
 #    eval sh $HOME/.config/base16-shell/base16-bright.light.sh
     set fish_function_path $fish_function_path "/home/john/.local/lib/python3.5/site-packages/powerline/bindings/fish"
     powerline-setup
